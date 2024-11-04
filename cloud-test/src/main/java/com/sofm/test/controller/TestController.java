1f56604489a9a86b4502c9e7925ce47004a8de28
txt <notreallyinatony@gmail.com>
2024-11-04 17:50:00 +0800

    redis 加入docker  控制feign调用超时时间

--- cloud-test/src/main/java/com/sofm/test/controller/TestController.java
+++ cloud-test/src/main/java/com/sofm/test/controller/TestController.java
@@ -1,13 +1,22 @@
 package com.sofm.test.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.serviceregistry.Registration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class TestController {
 
+    @Autowired(required = false)
+    private Registration registration; // 当前实例的注册信息
+
     @GetMapping(value = "/test/all")
-    public String test(){
-        return "hello test";
+    public String test() {
+        String instanceId = (registration != null) ? registration.getInstanceId() : "Unknown Instance ID";
+        log.info("request process in :{}", instanceId);
+        return "hello test" + instanceId;
     }
 }
